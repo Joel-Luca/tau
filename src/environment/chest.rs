f77f9d516213a0d4ab55eb0f9958607177a312eb
Joel-Luca <joel.christinet@gmx.ch>
2025-06-16 22:38:33 +0200

    Fix typo

--- src/environment/chest.rs
+++ src/environment/chest.rs
@@ -1,9 +1,9 @@
 use bevy::prelude::*;
 
 use crate::configuration::resolution::Resolution;
-use crate::physic::collision::Collision;
 use crate::physic::collision::collider::Collider;
 use crate::physic::collision::polygon::PolygonCollider;
+use crate::physic::collision::Collision;
 use crate::weapon::Weapon;
 
 #[derive(Component)]
@@ -27,11 +27,11 @@ impl ChestBundle {
         let weapon = Weapon::random();
         let chest_texture = assets_server.load(weapon.get_asset_name());
         let collider = PolygonCollider::new(Box::new([]));
-        let spwan_location = Transform::from_translation(position)
+        let spawn_location = Transform::from_translation(position)
             .with_scale(Vec3::splat(resolution.chest_pixel_ratio));
         ChestBundle {
             chest: Chest { weapon },
-            collider: Collision::new(Collider::Polygon(collider), spwan_location),
+            collider: Collision::new(Collider::Polygon(collider), spawn_location),
             sprite: Sprite::from_image(chest_texture),
         }
     }
