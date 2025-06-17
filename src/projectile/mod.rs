746238ed99b4e43f0cea74201c7352b6b0483954
Joel-Luca <joel.christinet@gmx.ch>
2025-06-17 21:16:01 +0200

    Remove unused check_player_collision
    The player collision is already checked in collision.py

--- src/projectile/mod.rs
+++ src/projectile/mod.rs
@@ -3,7 +3,6 @@ use bevy::prelude::*;
 use crate::physic::bounce::Bounce;
 use crate::physic::collision::collider::Collider;
 use crate::physic::solid::Solid;
-use crate::player::Player;
 
 pub mod bullet;
 pub mod mine;
@@ -13,27 +12,12 @@ pub struct ProjectilePlugin;
 
 impl Plugin for ProjectilePlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, (check_player_collision, check_solid_collision));
+        app.add_systems(Update, check_solid_collision);
     }
 }
 
 #[derive(Component)]
 pub struct Projectile {}
-
-fn check_player_collision(
-    mut commands: Commands,
-    player_query: Query<(Entity, &Collider), With<Player>>,
-    projectile_query: Query<(Entity, &Collider), With<Projectile>>,
-) {
-    for (player, player_c) in player_query.iter() {
-        for (bullet, projectile_) in projectile_query.iter() {
-            if player_c.intersects(projectile_) {
-                commands.entity(player).despawn();
-                commands.entity(bullet).despawn();
-            }
-        }
-    }
-}
 
 fn check_solid_collision(
     mut commands: Commands,
